ac292a72c6fe230961ffcf9cb8e1cdc9f6ed8534
blocktest-official <43646822+blocktest-official@users.noreply.github.com>
2019-03-25 13:30:20 -0400

    Update TestCompany.sol

--- TestCompany.sol
+++ TestCompany.sol
@@ -5,7 +5,7 @@ import "./Company.sol";
 contract TestCompany is Company {
     
     function testAddEmployee () public {
-		address employee = 0xCA35b7d915458EF540aDe6068dFe2F44E8fa733c;
+        address employee = 0xCA35b7d915458EF540aDe6068dFe2F44E8fa733c;
         if (isEmployee[employee]) {
             isEmployee[employee] = false;
         }
@@ -14,16 +14,33 @@ contract TestCompany is Company {
     }
     
     function testLogHour() public {
-		address employee = 0xCA35b7d915458EF540aDe6068dFe2F44E8fa733c;
-        uint totalHours = getHoursLogged(employee);
-        logHour(2);
-        assert(getHoursLogged(employee) == totalHours + 2);
+        address employee = 0xCA35b7d915458EF540aDe6068dFe2F44E8fa733c;
+        if (isEmployee[employee]){
+            uint totalHours = getHoursLogged(employee);
+            logHour(employee, 2);
+            assert(getHoursLogged(employee) == totalHours + 2);
+        }
+        else{
+            addEmployee(employee);
+            uint totalHourss = getHoursLogged(employee);
+            logHour(employee, 2);
+            assert(getHoursLogged(employee) == totalHourss + 2);
+        }   
+        
     }
     
     function testGetHoursLogged () public {
-		address employee = 0xCA35b7d915458EF540aDe6068dFe2F44E8fa733c;
-        uint totalHours = getHoursLogged(employee);
-        logHour(2);
-        assert(getHoursLogged(employee) == totalHours + 2);
+        address employee = 0xCA35b7d915458EF540aDe6068dFe2F44E8fa733c;
+        if (isEmployee[employee]){
+            uint totalHours = getHoursLogged(employee);
+            logHour(employee, 2);
+            assert(getHoursLogged(employee) == totalHours + 2);
+        }
+        else{
+            addEmployee(employee);
+            uint totalHourss = getHoursLogged(employee);
+            logHour(employee, 2);
+            assert(getHoursLogged(employee) == totalHourss + 2);
+        }
     }
 }
